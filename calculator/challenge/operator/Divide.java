87e357eca72b619ade2f1885df2d08fe96141f01
kimwanyoung <dhks2869@gmail.com>
2024-09-05 21:07:03 +0900

    fix : 공백 수정

--- calculator/challenge/operator/Divide.java
+++ calculator/challenge/operator/Divide.java
@@ -4,6 +4,6 @@ public class Divide extends AbstractOperator<Number> {
     @Override
     public Number operate(Number firstNumber, Number secondNumber) {
         if(secondNumber.equals(0)) throw new ArithmeticException("0으로 나눌 수 없습니다.");
-         return firstNumber.doubleValue() / secondNumber.doubleValue();
+        return firstNumber.doubleValue() / secondNumber.doubleValue();
     }
 }
